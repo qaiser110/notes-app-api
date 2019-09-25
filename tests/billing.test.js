4621c399f6bb39257f73b7de60ee019338d53d6f
Qaiser Abbas <Abbas.Qaiser@abc.net.au>
2019-09-25 21:39:27 +1000

    error check

--- tests/billing.test.js
+++ tests/billing.test.js
@@ -3,7 +3,7 @@ import { calculateCost } from "../libs/billing-lib";
 test("Lowest tier", () => {
   const storage = 10;
 
-  const cost = 4000;
+  const cost = 4001;
   const expectedCost = calculateCost(storage);
 
   expect(cost).toEqual(expectedCost);
